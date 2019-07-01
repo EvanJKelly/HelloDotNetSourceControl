2d17d6cc064db39925a97cc24d53ed43d1ce072d
evanjkelly123@gmail.com <evanjkelly123@gmail.com>
2019-07-01 14:23:08 -0700

    Added a greeting message

--- HelloDotNetSourceControl/Form1.cs
+++ HelloDotNetSourceControl/Form1.cs
@@ -16,5 +16,10 @@ namespace HelloDotNetSourceControl
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Console.WriteLine("hello world!");
+        }
     }
 }
